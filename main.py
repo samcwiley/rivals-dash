c9b26d9dc0740c3f926427ef4c82fa8918583ea6
Sam <samcwiley@gmail.com>
2024-12-12 14:47:20 -0500

    Removed most of the pandas function calls from main

--- main.py
+++ main.py
@@ -13,36 +13,12 @@ from df_utils_polars import *
 
 
 df_polars = parse_spreadsheet_polars("rivals_spreadsheet.tsv")
-df_pandas = parse_spreadsheet("rivals_spreadsheet.tsv")
-# print(df_polars.shape)
-# print(df_pandas.shape)
-# print(df_pandas.columns)
-# print(df_polars.columns)
-winrate_df = calculate_set_winrates(df_pandas)
+# df_pandas = parse_spreadsheet("rivals_spreadsheet.tsv")
+# winrate_df = calculate_set_winrates(df_pandas)
 winrate_df_polars = calculate_set_winrates_polars(df_polars)
-# print(winrate_df.head)
-# print(winrate_df_polars.head)
-gamewise_df = calculate_gamewise_df(df_pandas)
 gamewise_df_polars = calculate_gamewise_df_polars(df_polars)
-# print(gamewise_df.shape)
-# print(gamewise_df.head)
-# print(gamewise_df_polars.shape)
-# print(gamewise_df_polars.head)
-stage_winrate_df = calculate_stage_winrates(gamewise_df)
 stage_winrate_df_polars = calculate_stage_winrates_polars(gamewise_df_polars)
-# print(stage_winrate_df_polars.head)
-# print(stage_winrate_df.head)
 
-stage_bar = double_bar_plot(
-    title="Stage Winrates (pandas)",
-    x_axis=stage_winrate_df["Stage"],
-    y1_axis=stage_winrate_df["Total_Matches"],
-    y1_name="Number of Matches",
-    y1_axis_label="Frequency of Stage",
-    y2_axis=stage_winrate_df["WinRate"],
-    y2_name="Winrate",
-    y2_axis_label="Winrate",
-)
 
 stage_bar_polars = double_bar_plot_polars(
     title="Stage Winrates (polars)",
@@ -56,14 +32,6 @@ stage_bar_polars = double_bar_plot_polars(
 )
 
 
-elo_scatter = scatterplot_with_regression(
-    independent=df_pandas["My ELO"],
-    dependent=df_pandas["Opponent ELO"],
-    title="My ELO vs. Opponent ELO",
-    x_title="My ELO",
-    y_title="Opponent ELO",
-)
-
 elo_scatter_polars = scatterplot_with_regression_polars(
     independent=df_polars["My ELO"],
     dependent=df_polars["Opponent ELO"],
@@ -72,9 +40,6 @@ elo_scatter_polars = scatterplot_with_regression_polars(
     y_title="Opponent ELO",
 )
 
-stage_winrate_df["Stage_Width"] = stage_winrate_df["Stage"].map(
-    lambda stage: stages[stage].width
-)
 
 stages_df = pl.DataFrame(
     {
@@ -83,15 +48,6 @@ stages_df = pl.DataFrame(
     }
 )
 stage_winrate_df_polars = stage_winrate_df_polars.join(stages_df, on="Stage")
-
-
-stage_scatter = scatterplot_with_regression(
-    independent=stage_winrate_df["Stage_Width"],
-    dependent=stage_winrate_df["WinRate"],
-    title="Stage Width vs. Winrate (Pandas)",
-    x_title="Stage Width",
-    y_title="Winrate",
-)
 
 stage_scatter_polars = scatterplot_with_regression_polars(
     independent=stage_winrate_df_polars["Stage_Width"],
@@ -102,16 +58,6 @@ stage_scatter_polars = scatterplot_with_regression_polars(
 )
 
 # double bar graph for # matchups and winrate against each character
-matchup_bar = double_bar_plot(
-    title="Character Matchup Winrates (Pandas)",
-    x_axis=winrate_df["Main"],
-    y1_axis=winrate_df["Total_Matches"],
-    y1_name="Number of Matches",
-    y1_axis_label="Frequency of Opponent Characters",
-    y2_axis=winrate_df["WinRate"],
-    y2_name="Winrate",
-    y2_axis_label="Winrate",
-)
 
 matchup_bar_polars = double_bar_plot_polars(
     title="Character Matchup Winrates (Polars)",
@@ -131,28 +77,6 @@ char_options = ["All Characters"] + characters
 
 
 # Callback to update stage winrate plot
-@app.callback(Output("stage-bar-plot", "figure"), [Input("character-filter", "value")])
-def update_graph(selected_character):
-    if selected_character == "All Characters":
-        filtered_df = gamewise_df
-    else:
-        filtered_df = gamewise_df[gamewise_df["Char"] == selected_character]
-
-    stage_winrate_df = calculate_stage_winrates(filtered_df)
-
-    figure = double_bar_plot(
-        title=f"Stage Winrates Against {selected_character}",
-        x_axis=stage_winrate_df["Stage"],
-        y1_axis=stage_winrate_df["Total_Matches"],
-        y1_name="Number of Matches",
-        y1_axis_label="Frequency of Stage",
-        y2_axis=stage_winrate_df["WinRate"],
-        y2_name="Winrate",
-        y2_axis_label="Winrate",
-    )
-    return figure
-
-
 @app.callback(
     Output("stage-bar-plot-polars", "figure"), [Input("character-filter", "value")]
 )
@@ -181,16 +105,16 @@ app.layout = html.Div(
     [
         html.H1("ELO Analysis Dashboard"),
         # Line plot of ELO over time
-        dcc.Graph(
-            id="line-plot",
-            figure=px.line(
-                df_pandas,
-                x="Row Index",
-                y="My ELO",
-                title="My ELO Over Time",
-                labels={"Row Index": "Sets Played", "My ELO": "My ELO"},
-            ),
-        ),
+        # dcc.Graph(
+        # id="line-plot",
+        # figure=px.line(
+        # df_pandas,
+        # x="Row Index",
+        # y="My ELO",
+        # title="My ELO Over Time",
+        # labels={"Row Index": "Sets Played", "My ELO": "My ELO"},
+        # ),
+        # ),
         dcc.Graph(
             id="line-plot-polars",
             figure=px.line(
@@ -202,28 +126,18 @@ app.layout = html.Div(
             ),
         ),
         # Scatter plot of My ELO vs. Opponent ELO
-        dcc.Graph(id="scatter-plot", figure=elo_scatter),
-        # Same in polars
         dcc.Graph(id="elo-scatter-polars", figure=elo_scatter_polars),
         # Matchup bar plot
-        dcc.Graph(id="character-bar", figure=matchup_bar),
         dcc.Graph(id="character-bar-polars", figure=matchup_bar_polars),
         # stage winrate double bar plot
-        dcc.Graph(id="stage_winrate_double_plot", figure=stage_bar),
         dcc.Graph(id="stage_bar_polars", figure=stage_bar_polars),
-        dcc.Graph(
-            id="stage_winrate_scatter",
-            figure=stage_scatter,
-        ),
         dcc.Graph(id="stage_winrate_scatter_polars", figure=stage_scatter_polars),
-        # For filtering character bar plot
         dcc.Dropdown(
             id="character-filter",
             options=[{"label": char, "value": char} for char in char_options],
             value="All Characters",
             placeholder="Select a character",
         ),
-        dcc.Graph(id="stage-bar-plot"),
         dcc.Graph(id="stage-bar-plot-polars"),
     ]
 )
